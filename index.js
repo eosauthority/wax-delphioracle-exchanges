4029d00d7c45f89e63fbf62ce4a6885df0e753eb
EOS Authority <38717729+eosauthority@users.noreply.github.com>
2022-06-08 16:48:50 +0400

    Support fallback to multiple exchanges

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const {JsSignatureProvider} = require('eosjs/dist/eosjs-jssig');
 const fetch = require("node-fetch");
 const moment = require('moment');
 
+
 var rpc, signatureProvider, api;
 //try {
 //    rpc = new JsonRpc(config.endpoint, {fetch});
@@ -25,131 +26,166 @@ const pairs = [
 
 const send_quotes = async () => {
     console.log(`==============Start: ${moment().utc().format()}====================`)
-    console.log(`Exchange ${config.exchange}`)
-    if (!exchanges.includes(config.exchange)) {
-        console.log(`Exchange ${config.exchange} not in list`)
-        process.exit()
-    }
+//    console.log(`Exchange ${config.exchange}`)
+//    if (!exchanges.includes(config.exchange)) {
+//        console.log(`Exchange ${config.exchange} not in list`)
+//        process.exit()
+//    }
 
     try {
+        let fallbackExchanges = config.exchange.split(',');
+        let exchange = fallbackExchanges.shift();
+
         const quotes = [];
-        switch (config.exchange) {
-            case 'binance':
-                for (const pair of pairs) {
-                    var ticker;
-                    switch (pair.name) {
-                        case 'waxpbtc':
-                            ticker = 'WAXPBTC';
-                            break;
-                        case 'waxpusd':
-                            ticker = 'WAXPUSDT';
-                            break;
-                        default :
-                            ticker = null;
-                            break;
-                    }
-                    if (!ticker)
-                        continue;
+        do {
+            console.log(`Exchange ${exchange}`);
+            if (!exchanges.includes(exchange)) {
+                console.log(`Exchange ${exchange} not in list`)
+                process.exit()
+            }
 
-                    var url = `https://api.binance.com/api/v3/ticker/price?symbol=${ticker}`;
-                    const res = await fetch(url);
-                    const data = await res.json();
+            try {
+                switch (exchange) {
+                    case 'binance':
+                        for (const pair of pairs) {
+                            var ticker;
+                            switch (pair.name) {
+                                case 'waxpbtc':
+                                    ticker = 'WAXPBTC';
+                                    break;
+                                case 'waxpusd':
+                                    ticker = 'WAXPUSDT';
+                                    break;
+                                default :
+                                    ticker = null;
+                                    break;
+                            }
+                            if (!ticker)
+                                continue;
 
-                    if (!data || data.symbol != ticker || parseFloat(data.price) == 0)
-                        continue;
+                            var url = `https://api.binance.com/api/v3/ticker/price?symbol=${ticker}`;
+                            const res = await fetch(url, {timeout: 5 * 1000});
+                            if (!res.ok)
+                                continue;
 
-                    quotes.push({pair: pair.name, value: Math.round(parseFloat(data.price) * Math.pow(10, pair.precision))});
+                            const data = await res.json();
+
+                            if (!data || data.symbol != ticker || parseFloat(data.price) == 0)
+                                continue;
+
+                            quotes.push({pair: pair.name, value: Math.round(parseFloat(data.price) * Math.pow(10, pair.precision))});
+                        }
+                        break;
+                    case 'huobi':
+                        for (const pair of pairs) {
+                            var ticker;
+                            switch (pair.name) {
+                                case 'waxpbtc':
+                                    ticker = 'waxpbtc';
+                                    break;
+                                case 'waxpeth':
+                                    ticker = 'waxpeth';
+                                    break;
+                                case 'waxpusd':
+                                    ticker = 'waxpusdt';
+                                    break;
+                                default :
+                                    ticker = null;
+                                    break;
+                            }
+                            if (!ticker)
+                                continue;
+
+                            var url = `https://api.huobi.pro/market/trade?symbol=${ticker}`;
+                            const res = await fetch(url, {timeout: 5 * 1000});
+                            if (!res.ok)
+                                continue;
+
+                            const data = await res.json();
+
+                            if (!data || !data.tick || !data.tick.data[0] || parseFloat(data.tick.data[0].price) == 0)
+                                continue;
+
+                            quotes.push({pair: pair.name, value: Math.round(parseFloat(data.tick.data[0].price) * Math.pow(10, pair.precision))});
+                        }
+                        break;
+                    case 'bitfinex':
+                        for (const pair of pairs) {
+                            var ticker;
+                            switch (pair.name) {
+                                case 'waxpusd':
+                                    ticker = 'WAXUSD';
+                                    break;
+                                default :
+                                    ticker = null;
+                                    break;
+                            }
+
+                            if (!ticker)
+                                continue;
+
+                            var url = `https://api.bitfinex.com/v1/pubticker/${ticker}`;
+                            const res = await fetch(url, {timeout: 5 * 1000});
+                            if (!res.ok)
+                                continue;
+
+                            const data = await res.json();
+
+                            if (!data || parseFloat(data.last_price) == 0)
+                                continue;
+
+                            quotes.push({pair: pair.name, value: Math.round(parseFloat(data.last_price) * Math.pow(10, pair.precision))});
+
+                        }
+                        break;
+                    case 'bittrex':
+                        for (const pair of pairs) {
+                            var ticker;
+                            switch (pair.name) {
+                                case 'waxpbtc':
+                                    ticker = 'WAXP-BTC';
+                                    break;
+                                case 'waxpeth':
+                                    ticker = 'WAXP-ETH';
+                                    break;
+                                case 'waxpusd':
+                                    ticker = 'WAXP-USD';
+                                    break;
+                                default :
+                                    ticker = null;
+                                    break;
+                            }
+
+                            if (!ticker)
+                                continue;
+
+                            var url = `https://api.bittrex.com/v3/markets/${ticker}/ticker`;
+                            const res = await fetch(url, {timeout: 5 * 1000});
+                            if (!res.ok)
+                                continue;
+
+                            const data = await res.json();
+
+                            if (!data || data.symbol != ticker)
+                                continue;
+
+                            quotes.push({pair: pair.name, value: Math.round(parseFloat(data.lastTradeRate) * Math.pow(10, pair.precision))});
+
+                        }
+                        break;
                 }
-                break;
-            case 'huobi':
-                for (const pair of pairs) {
-                    var ticker;
-                    switch (pair.name) {
-                        case 'waxpbtc':
-                            ticker = 'waxpbtc';
-                            break;
-                        case 'waxpeth':
-                            ticker = 'waxpeth';
-                            break;
-                        case 'waxpusd':
-                            ticker = 'waxpusdt';
-                            break;
-                        default :
-                            ticker = null;
-                            break;
-                    }
-                    if (!ticker)
-                        continue;
+            } catch (e) {
+                console.error(`Failed Exchange ${exchange} - ${e.message}`);
+            }
 
-                    var url = `https://api.huobi.pro/market/trade?symbol=${ticker}`;
-                    const res = await fetch(url);
-                    const data = await res.json();
+            if (quotes.length === 0) {
+                exchange = fallbackExchanges.shift() || null;//Get next fallback exchange
+            } else {
+                exchange = null;//Got results set null skip while loop
+            }
 
-                    if (!data || !data.tick || !data.tick.data[0] || parseFloat(data.tick.data[0].price) == 0)
-                        continue;
+        } while (exchange);
 
-                    quotes.push({pair: pair.name, value: Math.round(parseFloat(data.tick.data[0].price) * Math.pow(10, pair.precision))});
-                }
-                break;
-            case 'bitfinex':
-                for (const pair of pairs) {
-                    var ticker;
-                    switch (pair.name) {
-                        case 'waxpusd':
-                            ticker = 'WAXUSD';
-                            break;
-                        default :
-                            ticker = null;
-                            break;
-                    }
-
-                    if (!ticker)
-                        continue;
-
-                    var url = `https://api.bitfinex.com/v1/pubticker/${ticker}`;
-                    const res = await fetch(url);
-                    const data = await res.json();
-
-                    if (!data || parseFloat(data.last_price) == 0)
-                        continue;
-
-                    quotes.push({pair: pair.name, value: Math.round(parseFloat(data.last_price) * Math.pow(10, pair.precision))});
-
-                }
-                break;
-            case 'bittrex':
-                for (const pair of pairs) {
-                    var ticker;
-                    switch (pair.name) {
-                        case 'waxpbtc':
-                            ticker = 'WAXP-BTC';
-                            break;
-                        case 'waxpeth':
-                            ticker = 'WAXP-ETH';
-                            break;
-                        case 'waxpusd':
-                            ticker = 'WAXP-USD';
-                            break;
-                        default :
-                            ticker = null;
-                            break;
-                    }
-
-                    if (!ticker)
-                        continue;
-
-                    var url = `https://api.bittrex.com/v3/markets/${ticker}/ticker`;
-                    const res = await fetch(url);
-                    const data = await res.json();
-
-                    if (!data || data.symbol != ticker)
-                        continue;
-
-                    quotes.push({pair: pair.name, value: Math.round(parseFloat(data.lastTradeRate) * Math.pow(10, pair.precision))});
-
-                }
-                break;
-        }
 
         if (quotes.length === 0) {
             throw new Error(`Quotes empty`);
